95526040bfac517474fba529b94d432429fe90c4
Rashmi Shreshtha <rashmi.shreshtha@gmail.com>
2020-06-09 09:30:57 -0700

    added comments

--- sql/person_query.sql
+++ sql/person_query.sql
@@ -1,4 +1,8 @@
-﻿SELECT
+﻿/*
+Write a SQL query for a report that provides the following information for 
+  each person in the Person table, regardless if there is an address for each of those people:				
+  */
+SELECT
   p.FirstName,
   p.LastName,
   a.City,
